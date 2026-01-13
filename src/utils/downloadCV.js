55c75b7e153b5341bdf0b829d1ce6a173d18e34d
Faikuzzaman-Rizvi <frrizvi11@gmail.com>
2026-01-14 03:09:43 +0600

    Update project code

--- src/utils/downloadCV.js
+++ src/utils/downloadCV.js
@@ -2,8 +2,7 @@ export const downloadCV = () => {
   const pdfUrl = "/Rizvi-cv-N.pdf";        
   const link = document.createElement("a");
   link.href = pdfUrl;
-  link.download = "Rizvi-cv-N.pdf";              
-  link.setAttribute("download", "");             
+  link.download = "Rizvi-cv-N.pdf";  // <-- set file name only once
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
